e142e13c7064fb47d7290db74814da0c8dacf79e
Momin-C <hellomomins@yahoo.com>
2020-04-03 13:46:56 -0400

    Modified ignores

---
+++ eclipse-workspace/CommandLineArguments/UsesModule.py
@@ -0,0 +1,4 @@
+#import mymath as ma
+from mymath import *
+print(sumOfNum(5,10))
+print(diffOfNum(10,5))
---
+++ eclipse-workspace/CommandLineArguments/mymath.py
@@ -0,0 +1,9 @@
+'''
+Created on Apr. 3, 2020
+
+@author: Momin Chaudhry
+'''
+def sumOfNum (x,y):
+    return x+y
+def diffOfNum (x,y):
+    return x-y
