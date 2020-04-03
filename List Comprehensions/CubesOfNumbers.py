860651e8254c016fd594311c06384d782c07f5b1
Momin-C <hellomomins@yahoo.com>
2020-04-03 14:58:40 -0400

    Added comments to test out if user is linked now

--- List Comprehensions/CubesOfNumbers.py
+++ List Comprehensions/CubesOfNumbers.py
@@ -3,5 +3,5 @@ for i in range(1,11):
     lst.append(i**3)
 print (lst)"""
 lst = []
-lst = [x**3 for x in range(1,11)]
+lst = [x**3 for x in range(1,11)] #This will x^3 everything from 1-11
 print (lst)
