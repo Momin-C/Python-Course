6ed4c13bad4154d78461a7159681f863c8db61c4
Momin-C <hellomomins@yahoo.com>
2020-04-24 16:36:05 -0400

    Finished Encapsulation

--- Encapsulation/Student.py
+++ Encapsulation/Student.py
@@ -1,8 +1,11 @@
 class Student:
-    def __int__(self):
-        self.id = 123
-        self.name = "John"
+    def __init__(self):
+        self.__id = 123
+        self.__name = "John"
+    def display (self):
+        print (self.__id)
+        print (self.__name)
 
-S = Student()
-print (S.id)
-print (S.name)
+s = Student()
+s.display()
+print(s._Student__id)
